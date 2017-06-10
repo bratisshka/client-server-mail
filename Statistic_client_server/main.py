cb27384ed23be58a9c002b9742af7068ffa886d4
Ruslan Nikonov <d-daimond-ru@yandex.ru>
2017-06-10 10:57:51 +0300

    Change from matplotlib to pandas

--- Statistic_client_server/main.py
+++ Statistic_client_server/main.py
@@ -57,41 +57,95 @@ UsersCur.close()
 
 n_groups = len(user_logins)
 
-means_men = all_messages_from
-means_women = all_messages_to
-means_deleted = all_messages_delete
-fig, ax = plt.subplots()
+#   plot
+# means_men = all_messages_from
+# means_women = all_messages_to
+# means_deleted = all_messages_delete
+# fig, ax = plt.subplots()
+#
+# index = np.arange(n_groups)
+# bar_width = 0.2
+#
+# opacity = 0.4
+# eror_config = {'ecolor': '0.3'}
+#
+# rects1 = plt.bar(index, means_men, bar_width, alpha=opacity, color='b',  error_kw=eror_config, label='From')
+# rects2 = plt.bar(index + bar_width, means_women, bar_width, alpha=opacity, color='r', error_kw=eror_config, label='To')
+# rects3 = plt.bar(index + bar_width*2, means_deleted, bar_width, alpha=opacity, color='g', error_kw=eror_config, label='Deleted')
+#
+# plt.xlabel('Users')
+# plt.ylabel('Count')
+# plt.title('Scotres by group and gender')
+# plt.xticks(index + bar_width / 2, user_logins)
+#
+# ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
+#
+# def autolabel(rects):
+#     for rect in rects:
+#         height = rect.get_height()
+#         ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+#                 '%d' % int(height),
+#                 ha='center', va='bottom')
+#
+# autolabel(rects1)
+# autolabel(rects2)
+# autolabel(rects3)
+#
+# plt.legend()
+#
+# plt.tight_layout()
+# plt.show()
 
-index = np.arange(n_groups)
-bar_width = 0.2
 
-opacity = 0.4
-eror_config = {'ecolor': '0.3'}
+#   plot_2
+from pandas import DataFrame
 
-rects1 = plt.bar(index, means_men, bar_width, alpha=opacity, color='b',  error_kw=eror_config, label='From')
-rects2 = plt.bar(index + bar_width, means_women, bar_width, alpha=opacity, color='r', error_kw=eror_config, label='To')
-rects3 = plt.bar(index + bar_width*2, means_deleted, bar_width, alpha=opacity, color='g', error_kw=eror_config, label='Deleted')
+change = all_messages_from
+user = user_logins
 
-plt.xlabel('Users')
-plt.ylabel('Count')
+grad = DataFrame({'change' : change, 'user': user})
+
+change = grad.change[grad.change > 0]
+user = grad.user[grad.change > 0]
+pos = np.arange(len(change))
+
 plt.title('Scotres by group and gender')
-plt.xticks(index + bar_width / 2, user_logins)
+plt.barh(pos, change)
+plt.xlabel('Count')
 
-ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
+#add the numbers to the side of each bar
+for p, c, ch in zip(pos, user, change):
+    plt.annotate(str(ch), xy=(ch + 1, p + .15), va='center')
 
-def autolabel(rects):
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
-                '%d' % int(height),
-                ha='center', va='bottom')
+#cutomize ticks
+ticks = plt.yticks(pos + .5, user)
+xt = plt.xticks()[0]
+plt.xticks(xt, [' '] * len(xt))
 
-autolabel(rects1)
-autolabel(rects2)
-autolabel(rects3)
+#minimize chartjunk
+def remove_border(axes=None, top=False, right=False, left=True, bottom=True):
+    ax = axes or plt.gca()
+    ax.spines['top'].set_visible(top)
+    ax.spines['right'].set_visible(right)
+    ax.spines['left'].set_visible(left)
+    ax.spines['bottom'].set_visible(bottom)
 
-plt.legend()
+    # turn off all ticks
+    ax.yaxis.set_ticks_position('none')
+    ax.xaxis.set_ticks_position('none')
 
-plt.tight_layout()
+    # now re-enable visibles
+    if top:
+        ax.xaxis.tick_top()
+    if bottom:
+        ax.xaxis.tick_bottom()
+    if left:
+        ax.yaxis.tick_left()
+    if right:
+        ax.yaxis.tick_right()
+
+#minimize chartjunk
+remove_border(left=False, bottom=False)
+plt.grid(axis = 'x', color ='white', linestyle='-')
+
 plt.show()
-
